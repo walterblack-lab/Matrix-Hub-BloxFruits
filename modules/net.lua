a87f238d007da58d0b1e443e1c967b005bbd079f
walterblack-lab <1akosbagan@gmail.com>
2026-02-10 22:58:19 +0100

    Update net.lua

--- modules/net.lua
+++ modules/net.lua
@@ -1,19 +1,13 @@
 local Net = {}
 local RS = game:GetService("ReplicatedStorage")
 
--- Biztonságos távoli esemény kereső
-local function getRemote(path)
-    local obj = RS
-    for _, name in pairs(path:split("/")) do
-        obj = obj:WaitForChild(name, 5)
-        if not obj then return nil end
-    end
-    return obj
+-- Atombiztos elérés
+local function getRemote()
+    return RS:WaitForChild("Modules"):WaitForChild("Net"):WaitForChild("RE/RegisterAttack")
 end
 
 Net.Remotes = {
-    Attack = getRemote("Modules/Net/RE/RegisterAttack"),
-    Quest = getRemote("Modules/Net/RF/StartSubclassQuest")
+    Attack = getRemote()
 }
 
 return Net
