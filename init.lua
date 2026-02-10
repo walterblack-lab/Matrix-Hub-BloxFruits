af2f2196e502b43f4ba7d757b6a0f84b27026e68
walterblack-lab <1akosbagan@gmail.com>
2026-02-10 22:10:46 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
+-- MATRIX HUB LOADER
+-- Description: Modular loader for the Blox Fruits project.
 
+local github = "https://raw.githubusercontent.com/FELHASZNALONEVED/Matrix-Hub-BloxFruits/main/"
+
+_G.Matrix_Modules = {
+    Net = loadstring(game:HttpGet(github .. "modules/net.lua"))(),
+    Tween = loadstring(game:HttpGet(github .. "modules/tween.lua"))(),
+    Farm = loadstring(game:HttpGet(github .. "modules/farm.lua"))()
+}
+
+loadstring(game:HttpGet(github .. "main.lua"))()
